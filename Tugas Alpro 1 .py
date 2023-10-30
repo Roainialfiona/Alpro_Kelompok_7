9d14abbdc14a733939f772b06fd1f1443cdb8fc6
111Roainialfiona <148738023+Roainialfiona@users.noreply.github.com>
2023-10-30 20:55:02 +0700

    Add files via upload

---
+++ Tugas Alpro 1 .py
@@ -0,0 +1,24 @@
+print("SELAMAT DATANG DI PIZZA HUT!!!")
+
+#Pilih Topping 
+print('//// Berikut Pilihan Topping ////')
+print('''-Frankuter
+-Meat Monsta
+-Super Supreme
+-Supeer Supreme Chicken''')
+
+Pilih_Topping = str(input("Pilih Menu Topping: "))
+if Pilih_Topping == 'Frankuter':
+    Harga_Topping = 43637
+    
+elif Pilih_Topping == 'Meat Monsta':
+    Harga_Topping = 43637
+
+elif Pilih_Topping == "Super Supreme": 
+    Harga_Topping = 43637
+    
+elif Pilih_Topping == "Super Supreme Chicken":
+    Harga_Topping = 43637
+    
+else :
+    print("Pilihan Menu Anda Tidak Valid")
